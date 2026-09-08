6c02b1a86579eada499652aafd90428eaf2b9303
SarraBee31 <sarraboughanmishc@gmail.com>
2026-09-08 19:45:44 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2026-03-01',
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: [
+    '@nuxt/image',
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    'shadcn-nuxt'
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "~/components/ui"
+     */
+    componentDir: '~/components/ui'
+  }
+})
